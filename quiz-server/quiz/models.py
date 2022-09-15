1f19b5a5496de9dc2aefd50faa0d92ba880120a5
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-15 08:33:33 +0300

    create Category and Quiz models

--- quiz-server/quiz/models.py
+++ quiz-server/quiz/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Category(TimeStampedModel):
+    title = models.CharField(max_length=155)
+
+
+class Quiz(TimeStampedModel):
+    title = models.CharField(max_length=155)
+    description = models.TextField(null=True, blank=True)
+    pass_mark = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)
