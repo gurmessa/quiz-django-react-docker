0282d6d3957fa3d3a6efd942b36d415bdabd9bcb
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-15 09:17:46 +0300

    create Question and Answer models

--- quiz-server/quiz/models.py
+++ quiz-server/quiz/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q, UniqueConstraint
 
 class TimeStampedModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
@@ -18,3 +19,20 @@ class Quiz(TimeStampedModel):
     pass_mark = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     is_published = models.BooleanField(default=False)
+
+
+class Question(TimeStampedModel):
+    text = models.CharField(unique=True, max_length=255)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+
+class Answer(TimeStampedModel):
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="answers")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['is_correct', 'question'], condition=Q(is_correct=True), name='unique_correct_answer')
+        ]
+        unique_together = ('text', 'question',)
