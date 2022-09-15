2141473d8a0135f192bb9cf43514f982989fedb4
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-15 09:17:35 +0300

    test Question and Answer models

--- quiz-server/quiz/tests/test_model.py
+++ quiz-server/quiz/tests/test_model.py
@@ -1,5 +1,6 @@
-from unicodedata import category
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 from quiz.models import *
 
 
@@ -48,3 +49,92 @@ class CategoryAndQuizModelTest(TestCase):
         self.assertEqual(second_saved_quiz.description, None)
 
 
+
+class QuestionAndAnswerModelTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(
+            title="Sport"
+        )
+        self.quiz = Quiz.objects.create(
+            title="Basketball",
+            category=category,
+            pass_mark=6,
+        )
+
+    def test_saving_question_and_answers(self):
+        question = Question.objects.create(
+            text = "Questions one ..",
+            quiz=self.quiz
+        )
+
+        answer1 = Answer.objects.create(
+            question=question,
+            is_correct=True,
+            text="Answer One"
+        )
+
+        answer2 = Answer.objects.create(
+            question=question,
+            text="Answer two"
+        )
+
+        answer3 = Answer.objects.create(
+            question=question,
+            text="Answer three"
+        )
+
+        saved_question = Question.objects.last()
+        self.assertEqual(saved_question, question)
+
+        saved_answers = Answer.objects.all()        
+        self.assertEqual(saved_answers.count(), 3)
+        self.assertEqual(saved_question.answers.count(), 3)
+        self.assertIn(answer1, saved_answers)
+        self.assertIn(answer2, saved_answers)
+        self.assertIn(answer3, saved_answers)
+
+    def test_duplicate_questions_are_invalid(self):
+        Question.objects.create(
+            text = "Questions one ..",
+            quiz=self.quiz
+        )
+
+        with self.assertRaises(IntegrityError):
+            Question.objects.create(
+                text = "Questions one ..",
+                quiz=self.quiz
+            )
+
+    def test_duplicate_answers_are_invalid(self):
+        question = Question.objects.create(
+            text = "Questions one ..",
+            quiz=self.quiz
+        )
+
+        Answer.objects.create(
+            question=question,
+            text="Answer one"
+        )
+
+        with self.assertRaises(IntegrityError):
+            Answer.objects.create(
+                question=question,
+                text="Answer one"
+            )
+
+    def test_cannot_save_multiple_correct_answers(self):
+        question = Question.objects.create(
+            text = "Questions one ..",
+            quiz=self.quiz
+        )
+
+        Answer.objects.create(
+            question=question,
+            text="Answer one"
+        )
+
+        with self.assertRaises(IntegrityError):
+            Answer.objects.create(
+                question=question,
+                text="Answer one"
+            )
