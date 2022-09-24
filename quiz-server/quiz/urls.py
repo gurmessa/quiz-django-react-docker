2432bb02fead081f52ad7a558dc20022ad341cf8
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-24 20:48:57 +0300

    implement API for registering for a quiz

--- quiz-server/quiz/urls.py
+++ quiz-server/quiz/urls.py
@@ -1,6 +1,8 @@
 
 from django.urls import path
 from quiz import views
+from quiz.models import TakenQuiz
 urlpatterns = [
-    path("quizzes", views.QuizListAPIView.as_view(),name="quizzes")
+    path("quizzes", views.QuizListAPIView.as_view(),name="quizzes"),
+    path("quiz/<pk>/register", views.TakenQuizAPIView.as_view(),name="quizzes")
 ]
--- quiz-server/quiz/views.py
+++ quiz-server/quiz/views.py
@@ -1,6 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from quiz.models import Quiz
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from quiz.models import Quiz, TakenQuiz
 from quiz.serializers import QuizSerializer
 
 
@@ -8,3 +12,23 @@ class QuizListAPIView(generics.ListAPIView):
     queryset = Quiz.objects.published()
     serializer_class = QuizSerializer
     permission_classes = [IsAuthenticated, ]
+
+
+class TakenQuizAPIView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request, *args, **kwargs):
+        quiz = get_object_or_404(Quiz, pk=kwargs['pk'])
+        if not TakenQuiz.objects.filter(
+            user=self.request.user,
+            quiz=quiz
+        ).exists():
+            taken_quiz = TakenQuiz.objects.create(
+                user=self.request.user,
+                quiz=quiz
+            )
+            return Response({
+                "id":taken_quiz.pk,
+                "message": "Registered successfully"
+            }, status=status.HTTP_201_CREATED)
+        return Response({"message": "You cannot taken a quiz twice"}, status=status.HTTP_400_BAD_REQUEST)
