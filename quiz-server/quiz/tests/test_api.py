10614b057b7082980eafa1739c2546ca1953b905
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-24 20:07:49 +0300

    test for quiz list api

--- quiz-server/quiz/tests/test_api.py
+++ quiz-server/quiz/tests/test_api.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework import status
+from quiz.factories import *
+
 
 class AuthenticationTest(APITestCase):
     def test_user_can_login(self):
@@ -36,4 +38,56 @@ class AuthenticationTest(APITestCase):
         self.assertEqual(user.email, email)
         self.assertEqual(user.username, username)  
 
-      
+
+class QuizApiTest(APITestCase):
+    def setUp(self):
+        self.PASSWORD = "PassWord"
+        self.user = UserFactory(password=self.PASSWORD)
+
+        
+    def test_cannot_list_quizzes_for_unauthenticated_user(self):
+        response = self.client.get('/api/quizzes')
+
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+
+    def test_user_can_list_quizzes(self):
+        self.client.login(username=self.user.username, password=self.PASSWORD)  
+
+        quiz1 = QuizFactory()
+        quiz2 = QuizFactory()
+        quiz3 = QuizFactory()
+
+        TakenQuiz.objects.create(
+            user=self.user,
+            quiz=quiz3
+        )
+
+        response = self.client.get('/api/quizzes')
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        exp_quiz_ids = [quiz1.id, quiz2.id, quiz3.id]
+        act_quiz_ids = [quiz.get('id') for quiz in response.data]
+        self.assertCountEqual(act_quiz_ids, exp_quiz_ids)
+
+        exp_quiz1 = response.data[0]
+
+        self.assertEqual(quiz1.title, exp_quiz1['title'])
+        self.assertEqual(quiz1.description, exp_quiz1['description'])
+        self.assertEqual(quiz1.category.title, exp_quiz1['category'])
+        self.assertFalse(exp_quiz1['taken'])
+        
+
+        exp_quiz3 = response.data[2]
+        self.assertTrue(exp_quiz3['taken'])
+
+
+    def test_unpublished_quizzes_arenot_listed(self):
+        self.client.login(username=self.user.username, password=self.PASSWORD)  
+
+        quiz1 = QuizFactory()
+        quiz2 = QuizFactory(is_published=False)
+
+        response = self.client.get('/api/quizzes')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(1, len(response.data))
