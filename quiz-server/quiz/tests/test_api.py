cdee5b6072f514445188fd5c28c5e06720aa6c86
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-09-27 07:33:37 +0300

    write test for next-question API

--- quiz-server/quiz/tests/test_api.py
+++ quiz-server/quiz/tests/test_api.py
@@ -122,3 +122,102 @@ class TakenQuizApiTest(APITestCase):
         self.assertEqual(status.HTTP_400_BAD_REQUEST, response.status_code)
 
     
+class QuizNextQuestionApiTest(APITestCase):
+    def setUp(self):
+        self.PASSWORD = "PassWord"
+        self.user1 = UserFactory(password=self.PASSWORD)
+        self.user2 = UserFactory(password=self.PASSWORD)
+        self.quiz1 = QuizFactory()
+
+
+        self.question1 = QuestionFactory(quiz=self.quiz1)
+        AnswerFactory(is_correct=True, question=self.question1)
+        AnswerFactory(question=self.question1)
+        AnswerFactory(question=self.question1)
+
+        self.question2 = QuestionFactory(quiz=self.quiz1)
+        AnswerFactory(question=self.question2)
+        AnswerFactory(is_correct=True, question=self.question2)
+        AnswerFactory(question=self.question2)
+
+        self.taken_quiz = TakenQuiz.objects.create(
+            user=self.user1,
+            quiz=self.quiz1,
+            completed=True,
+        )
+
+        self.questions = [self.question1, self.question2]
+
+    def test_unauthorized_user_cannot_get_next_question(self):
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(status.HTTP_403_FORBIDDEN, response.status_code)
+
+    def test_authorized_user_can_get_next_question(self):
+        self.client.login(username=self.user1.username, password=self.PASSWORD)  
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        exp_attempted_question = AttemptedQuestion.objects.last()
+       
+        self.assertEqual(exp_attempted_question.taken_quiz, self.taken_quiz)
+        self.assertEqual(exp_attempted_question.question, self.question1)
+        self.assertIsNone(exp_attempted_question.answer)
+
+        self.assertEqual(
+            response.data['id'],
+            self.taken_quiz.id
+        )
+        self.assertEqual(
+            response.data['question']['text'],
+            self.question1.text
+        )
+        self.assertEqual(
+            response.data['question']['answers'],
+            [{'id': answer.id, 'text':answer.text} for answer in self.question1.answers.all()]
+        )
+        self.assertTrue(response.data['has_next_question'])
+
+        # get second question
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+
+        exp_attempted_question2 = AttemptedQuestion.objects.last()
+       
+        self.assertEqual(exp_attempted_question2.taken_quiz, self.taken_quiz)
+        self.assertEqual(exp_attempted_question2.question, self.question2)
+        self.assertIsNone(exp_attempted_question2.answer)
+
+        self.assertEqual(
+            response.data['id'],
+            exp_attempted_question2.id
+        )
+        self.assertEqual(
+            response.data['question']['text'],
+            self.question2.text
+        )
+        self.assertEqual(
+            response.data['question']['answers'],
+            [{'id': answer.id, 'text':answer.text} for answer in self.question2.answers.all()]
+        )
+        self.assertFalse(response.data['has_next_question'])
+
+
+    def test_get_next_question_raises_exception_no_next_question(self):
+        self.client.login(username=self.user1.username, password=self.PASSWORD)  
+        # get first question
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+       
+        # get second question
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        
+        # raise exception since there is no 3rd question
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+       
+    def test_user_cannot_access_other_users_quiz(self):
+        self.client.login(username=self.user2.username, password=self.PASSWORD)
+
+        response = self.client.get(f'/api/quiz/{self.taken_quiz.id}/next-question')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+       
