eb4a6f41ee4b68558b3d685b1a8e458f8f561431
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-18 07:18:10 +0300

    test login page

--- quiz-client/cypress/e2e/authentication.cy.js
+++ quiz-client/cypress/e2e/authentication.cy.js
@@ -1,5 +1,33 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+const login = () => {
+  cy.intercept(
+    'POST',
+    '**/api/auth/login',
+    {
+      statusCode: 200,
+      body: {
+        'key': 'ACCESS_TOKEN'
+      }
+    }
+  ).as('login');
+
+  cy.visit('/login');
+  cy.get('[data-cy=input-username]').type('test');
+  cy.get('[data-cy=input-password]').type('test', { log: false });
+  cy.get('button').contains('Login').click();
+  cy.wait('@login');
+}
+
+describe('Authentication', () => {
+  it('Can login', () => {
+    login()    
+    cy.location('pathname').should('eq', '/')
   })
+
+  it('Cannot visit the login page when logged in.', function () {
+    login()
+
+    cy.visit('/login');
+    cy.location('pathname').should('eq', '/');
+  });
 })
