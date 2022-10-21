f94d204eb67c11a7a6670e0e149ee60495b6196b
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-21 22:28:06 +0300

    functional tests for sign up & login pages

--- quiz-client/cypress/e2e/authentication.cy.js
+++ quiz-client/cypress/e2e/authentication.cy.js
@@ -30,4 +30,56 @@ describe('Authentication', () => {
     cy.visit('/login');
     cy.location('pathname').should('eq', '/');
   });
+
+  it('Can sign up.', function () {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: '**/api/auth/registration/',
+      status: 201,
+      response: {
+        'key': '123456',
+      }
+    }).as('signUp');
+  
+    cy.visit('/signup');
+    cy.get('[data-cy=input-username]').type('test');
+    cy.get('[data-cy=input-email]').type('test@example.com');
+    cy.get('[data-cy=input-password1]').type('pAssw0rd');
+    cy.get('[data-cy=input-password2]').type('pAssw0rd');
+    
+    cy.get('button').contains('Create Account').click();
+    cy.wait('@signUp');
+    cy.location('pathname').should('eq', '/login');
+  });
+
+  it('Show invalid fields on sign up error.', function () {
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: '**/api/auth/registration/',
+      status: 400,
+      response: {
+        'username': [
+          'A user with that username already exists.'
+        ]
+      }
+    }).as('signUp');
+    cy.visit('/signup');
+    cy.get('[data-cy=input-username]').type('test');
+    cy.get('[data-cy=input-email]').type('test@example.com');
+    cy.get('[data-cy=input-password1]').type('pAssw0rd');
+    cy.get('[data-cy=input-password2]').type('pAssw0rd');;
+
+    cy.get('button').contains('Create Account').click();
+    cy.wait('@signUp');
+
+    cy.get('span').contains(
+      'A user with that username already exists'
+    );
+
+    cy.location('pathname').should('eq', '/signup');
+    
+  });
+
 })
