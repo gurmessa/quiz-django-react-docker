aa755958fe4f20ee4495aadb3b2d997a2947e25f
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-21 22:21:57 +0300

    update App.js & Home.js

--- quiz-client/src/App.js
+++ quiz-client/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 import Home from './pages/Home';
 import { getLocalToken } from './utils/auth_utils';
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={isLoggedIn ? <Navigate to='/' /> : <Login/>} />
+        <Route path='/signup' element={isLoggedIn ? <Navigate to='/' /> : <Signup/>} />
       </Routes>
     </div>
   );
