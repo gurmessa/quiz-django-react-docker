28e7f3167166b80f5208210bbdc11e37dfbc0637
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-23 13:24:47 +0300

    implement searching quizzes by category & quiz title

--- quiz-client/src/pages/Home.js
+++ quiz-client/src/pages/Home.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react"
-import { getQuizzes } from "../services/QuizSerivce"
+
+import { getQuizzes, getCategories } from "../services/QuizSerivce"
 import QuizCard from "../components/QuizCard";
-import SearchComponent from "../components/SearchComponent";
+import SearchQuizzes from "../components/SearchQuizzes";
+import CategorySelector from "../components/CategorySelector";
+
+const ALL_CATEGORY_ITEM = {title:"All", id:'all'}
 
 export default function Home(){
     const [quizzes, setQuizzes] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(ALL_CATEGORY_ITEM)
 
-    const loadQuizzes = async (term=null) => {
+    const loadQuizzes = async (term=null,category=null) => {
         setQuizzes([]);
-        const { response, isError } = await getQuizzes(term);
+        const { response, isError } = await getQuizzes(term,category);
         if (isError) {
 
         }else{
@@ -17,20 +24,55 @@ export default function Home(){
 
     }
 
-    const handleTermChange = (term) => {
-        loadQuizzes(term)
-    }
     useEffect(()=>{
         loadQuizzes()
     }, [])
 
+    useEffect(()=>{
+        if(selectedCategory.id !== 'all'){
+            loadQuizzes(searchTerm, selectedCategory.id)
+        }else{
+            loadQuizzes(searchTerm)
+        }
+        
+    }, [selectedCategory, searchTerm])
+
+
+    useEffect(()=>{
+        const loadCategories = async () => {
+            const { response, isError } = await getCategories();
+            if (isError) {
+    
+            }else{
+                setCategories([ALL_CATEGORY_ITEM, ...response.data])
+            }
+        }
+
+        loadCategories()
+    }, [])
+
+
     return (<div className="bg-gray-100 h-screen">
         <div className="max-w-5xl mx-auto">
             <h1 class="font-bold text-2xl sm:text-3xl py-5 px-3">Take Quizzes</h1>
-            <SearchComponent handleTermChange={handleTermChange}/>
-            <div class="flex mx-auto flex-wrap">
+            <div className="flex gap-2">
+               <CategorySelector 
+                    categories={categories}
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory}
+                />
+                <br />
+                <br />
+                <SearchQuizzes 
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}/>
+                </div>
+            <div class="flex mx-auto flex-wrap mt-8">
                 {quizzes.map(quiz => <QuizCard quiz={quiz} />)}
             </div>
         </div>
+
+
+
     </div>)
 }
