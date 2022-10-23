de7fd6fb2e4a595211486b7f94fc99e412a550d4
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-23 13:23:59 +0300

    API service for getting categories, update getQuizzes

--- quiz-client/src/services/QuizSerivce.js
+++ quiz-client/src/services/QuizSerivce.js
@@ -3,16 +3,36 @@ import { getLocalToken } from '../utils/auth_utils';
 
 const base_url = process.env.REACT_APP_API_URI
 
-export const getQuizzes = async (term=null) => {
+export const getQuizzes = async (term=null,category=null) => {
     let url = base_url+'quizzes';
-    if(term){
-        url += `?title=${term}`
+    if(term || category){
+        url += '?';
+
+        if(term){
+            url += `&title=${term}`
+        }
+        if(category){
+            url += `&category=${category}`
+        }
     }
+
     const token = getLocalToken()
     const headers = { Authorization: `Token ${token}` };
 
     try {
         const response = await axios.get(url, { headers });
+        return { response, isError: false };
+    } catch (response) {
+        return { response, isError: true };
+    }
+  };
+
+
+  export const getCategories = async () => {
+    let url = base_url+'categories';
+    
+    try {
+        const response = await axios.get(url, );
         return { response, isError: false };
     } catch (response) {
         return { response, isError: true };
