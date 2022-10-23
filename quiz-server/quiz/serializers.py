1d6a079a44fbedf7475bcb03087d6b5a0dd201ba
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-23 11:15:46 +0300

    config drf authentication classes

--- quiz-server/quiz/serializers.py
+++ quiz-server/quiz/serializers.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from rest_framework import serializers
 from quiz.models import Quiz, TakenQuiz, Question, Answer, AttemptedQuestion
 
--- quiz-server/quizserver/settings.py
+++ quiz-server/quizserver/settings.py
@@ -149,3 +149,10 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
