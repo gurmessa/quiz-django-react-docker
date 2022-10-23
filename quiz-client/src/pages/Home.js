e72aa1b4a169e91cdfb95f73310ad824beb7861c
Gurmessa Lemma <gurmessalemma@gmail.com>
2022-10-23 11:13:18 +0300

    implement quiz list & search

--- quiz-client/src/pages/Home.js
+++ quiz-client/src/pages/Home.js
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react"
+import { getQuizzes } from "../services/QuizSerivce"
+import QuizCard from "../components/QuizCard";
+import SearchComponent from "../components/SearchComponent";
+
 export default function Home(){
-    return (
-        <h1>Home</h1>
-    )
+    const [quizzes, setQuizzes] = useState([]);
+
+    const loadQuizzes = async (term=null) => {
+        setQuizzes([]);
+        const { response, isError } = await getQuizzes(term);
+        if (isError) {
+
+        }else{
+            setQuizzes(response.data)
+        }
+
+    }
+
+    const handleTermChange = (term) => {
+        loadQuizzes(term)
+    }
+    useEffect(()=>{
+        loadQuizzes()
+    }, [])
+
+    return (<div className="bg-gray-100 h-screen">
+        <div className="max-w-5xl mx-auto">
+            <h1 class="font-bold text-2xl sm:text-3xl py-5 px-3">Take Quizzes</h1>
+            <SearchComponent handleTermChange={handleTermChange}/>
+            <div class="flex mx-auto flex-wrap">
+                {quizzes.map(quiz => <QuizCard quiz={quiz} />)}
+            </div>
+        </div>
+    </div>)
 }
